91852ca96b8cfe996e1f62b096cca8f0390b2e9d
Evestar <evertjan9@gmail.com>
2015-03-23 17:06:49 +0100

    Start of project

---
+++ Clarity/app/src/main/java/com/riftwalkers/clarity/MainActivity.java
@@ -0,0 +1,37 @@
+package com.riftwalkers.clarity;
+
+import android.os.Bundle;
+
+import com.metaio.sdk.ARViewActivity;
+import com.metaio.sdk.jni.IGeometry;
+import com.metaio.sdk.jni.IMetaioSDKCallback;
+
+
+public class MainActivity extends ARViewActivity {
+
+    @Override
+    protected int getGUILayout() {
+        return 0;
+    }
+
+    @Override
+    protected IMetaioSDKCallback getMetaioSDKCallbackHandler() {
+        return null;
+    }
+
+    @Override
+    protected void loadContents() {
+
+    }
+
+    @Override
+    protected void onGeometryTouched(IGeometry geometry) {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
