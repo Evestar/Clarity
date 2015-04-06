3ce895da5dea86a124c1150e1e135e1177bd3e34
Mjollnir94 <marcel_hollink@hotmail.com>
2015-04-06 14:37:04 +0200

    Slider menu - no text
    implemented a hamburger slider menu with inner listview for legend items

--- Clarity/app/src/main/java/com/riftwalkers/clarity/MainActivity.java
+++ Clarity/app/src/main/java/com/riftwalkers/clarity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.riftwalkers.clarity;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.metaio.sdk.ARViewActivity;
 import com.metaio.sdk.jni.IGeometry;
@@ -11,7 +14,7 @@ public class MainActivity extends ARViewActivity {
 
     @Override
     protected int getGUILayout() {
-        return 0;
+        return R.layout.activity_inapp;
     }
 
     @Override
@@ -32,6 +35,25 @@ public class MainActivity extends ARViewActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_inapp);
+
+        createListView();
+    }
+
+    public void createListView() {
+        //Create list of items
+        String[] items = {"Meerpalen", "Schepen", "Incidenten"};
+
+        // build Adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                R.layout.listview_items,
+                items
+        );
+
+        // Config the list view
+        ListView list = (ListView) findViewById(R.id.legend);
+
+        list.setAdapter(adapter);
     }
 }
