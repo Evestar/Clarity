538b8ab5f8a175fc3ea6994c7ca71f768f7f40b8
jorikito <jorikpaul@hotmail.com>
2015-04-13 21:16:13 +0200

    Different POI's test

--- Clarity/app/src/main/java/com/riftwalkers/clarity/MainActivity.java
+++ Clarity/app/src/main/java/com/riftwalkers/clarity/MainActivity.java
@@ -15,6 +15,8 @@ import android.widget.ListView;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.metaio.sdk.ARViewActivity;
 import com.metaio.sdk.MetaioDebug;
@@ -23,8 +25,37 @@ import com.metaio.sdk.jni.IMetaioSDKCallback;
 import com.metaio.sdk.jni.LLACoordinate;
 import com.metaio.tools.io.AssetsManager;
 
+import org.json.JSONArray;
+
 
 public class MainActivity extends ARViewActivity {
+
+enum PoiType {
+    Schip,
+    Bolder,
+    Boei
+}
+
+
+class PointOfInterest {
+    int Id;
+    PoiType Type;
+    String Description;
+    LLACoordinate Coordinate;
+
+    String GetImageName ()
+    {
+        switch (this.Type){
+            case Schip: return "POIc.png";
+            case Boei: return "POIb.png";
+            case Bolder: return "ExamplePOI.png";
+            default: return "ExamplePOI.png";
+        }
+    }
+}
+
+
+    //TIJDELIJKE JSON MET DUMMY DATA, WORDT LATER UIT JSON FILES GEHAALD
 
     // Drawer Variables
     private String[] drawerItems;
@@ -32,7 +63,8 @@ public class MainActivity extends ARViewActivity {
     private ActionBarDrawerToggle drawerToggle;
     private ListView drawerList;
 
-    private IGeometry vuurtoren,standbeeld,blaak,havenBedrijf;
+    //private IGeometry vuurtoren,standbeeld,blaak,havenBedrijf;
+    private ArrayList<PointOfInterest> pointOfInterestList;
 
     @Override
     protected int getGUILayout() {
@@ -111,19 +143,53 @@ public class MainActivity extends ARViewActivity {
         metaioSDK.setLLAObjectRenderingLimits(5, 200);
         metaioSDK.setRendererClippingPlaneLimits(10, 220000);
 
-        LLACoordinate vuurtorenLLA = new LLACoordinate(51.916801, 4.482380, 0, 0);
-        LLACoordinate blaakLLA = new LLACoordinate(51.919855, 4.489412, 0, 0);
-        LLACoordinate standbeeldLLA = new LLACoordinate(51.917648, 4.483057, 0, 0);
-        LLACoordinate havenBedrijfLLA = new LLACoordinate(51.904794,4.484548, 0, 0);
+        // Replace this by converting JSON to the object model
+        pointOfInterestList = new ArrayList<PointOfInterest>();
 
-        File POIbackground = AssetsManager.getAssetPathAsFile(getApplicationContext(), "ExamplePOI.png");
+        PointOfInterest schip1 = new PointOfInterest();
+        schip1.Id =  1;
+        schip1.Description = "schip info x meter";
+        schip1.Coordinate = new LLACoordinate(51.916801, 4.482380, 0, 0);
+        schip1.Type = PoiType.Schip;
+        pointOfInterestList.add(schip1);
 
-        if(POIbackground != null) {
-            createGeometry(vuurtoren, vuurtorenLLA, POIbackground, 100);
-            createGeometry(blaak, blaakLLA, POIbackground, 100);
-            createGeometry(standbeeld, standbeeldLLA, POIbackground, 100);
-            createGeometry(havenBedrijf, havenBedrijfLLA, POIbackground, 100);
+        PointOfInterest bolder1 = new PointOfInterest();
+        bolder1.Id =  2;
+        bolder1.Description = "bolder info x meter";
+        bolder1.Coordinate = new LLACoordinate(51.919855, 4.489412, 0, 0);
+        bolder1.Type = PoiType.Bolder;
+        pointOfInterestList.add(bolder1);
+
+        PointOfInterest bolder2 = new PointOfInterest();
+        bolder2.Id =  3;
+        bolder2.Description = "bolder info x meter";
+        bolder2.Coordinate = new LLACoordinate(51.917648, 4.483057, 0, 0);
+        bolder2.Type = PoiType.Bolder;
+        pointOfInterestList.add(bolder2);
+
+        PointOfInterest boei1 = new PointOfInterest();
+        boei1.Id =  4;
+        boei1.Description = "boei info x meter";
+        boei1.Coordinate = new LLACoordinate(51.904794,4.484548, 0, 0);
+        boei1.Type = PoiType.Boei;
+        pointOfInterestList.add(boei1);
+
+        //AssetsManager.extractAllAssets(this, true);
+
+        //draw each poi in the arrayList
+        for(int i=0;i<pointOfInterestList.size();i++) {
+
+            //get type of POI and image
+            File POIbackground = AssetsManager.getAssetPathAsFile(getApplicationContext(), pointOfInterestList.get(i).GetImageName());
+            //File POIbackground = AssetsManager.getAssetPathAsFile(getApplicationContext(), "ExamplePOI.png");
+            String name = Integer.toString(pointOfInterestList.get(i).Id);
+            IGeometry geometry = null;
+            if(POIbackground != null) {
+                createGeometry(geometry, pointOfInterestList.get(i).Coordinate, POIbackground, 100);
+            }
         }
+
+
     }
 
     /**
