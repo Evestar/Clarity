db8dbe84d8353f1eabe0e59fff2dfe6fb061f2f1
Mjollnir94 <marcel_hollink@hotmail.com>
2015-05-01 12:13:28 +0200

    Optimize rest

--- Clarity/app/src/main/java/com/riftwalkers/clarity/RoleSelector.java
+++ Clarity/app/src/main/java/com/riftwalkers/clarity/RoleSelector.java
@@ -1,11 +1,8 @@
 package com.riftwalkers.clarity;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -55,7 +52,7 @@ public class RoleSelector extends Activity {
                 public void finished() {
                     changeLayout(editor);
                     editor.putBoolean("SETUPCOMPLETED", true);
-                    editor.commit();
+                    editor.apply();
                     Toast.makeText(getApplicationContext(), "All assets are loaded", Toast.LENGTH_SHORT).show();
                 }
             });
